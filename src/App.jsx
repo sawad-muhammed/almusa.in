e6bd163050d29985b76e7fd8c00612fa9939541a
muhammed-sawad-egov <muhammed.sawad@egov.global>
2026-07-12 17:32:08 +0530

    Initialize React app with Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>Coming Soon</h1>
+        <p>We're working on something amazing.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;

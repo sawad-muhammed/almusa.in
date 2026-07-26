20f6b6921d231d38dedf733209c810eb8cd763ef
muhammed-sawad-egov <muhammed.sawad@egov.global>
2026-07-26 20:24:11 +0530

    Updated workflow & config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
@@ -14,23 +17,3 @@ export default defineConfig({
     open: true,
   },
 })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import { resolve } from 'path'
-
-// import { cloudflare } from "@cloudflare/vite-plugin";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), cloudflare()],
-//   resolve: {
-//     alias: {
-//       '@': resolve(__dirname, 'src'),
-//     },
-//   },
-//   server: {
-//     port: 5173,
-//     open: true,
-//   },
-// })
